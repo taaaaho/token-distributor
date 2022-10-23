834f0c24fed896aa5423ef5499b9f5df407d0fb9
taaaaho <taaaaho@gmail.com>
2022-10-23 18:49:29 +0900

    feat: middleware to localhost

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,12 +1,18 @@
 import next from 'next'
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
 export default function middleware(req: {
   nextUrl: { pathname: any; search: string; origin: string }
   headers: { get: (arg0: string) => string }
 }) {
+  // console.log(req.nextUrl)
   const { pathname, origin, search } = req.nextUrl
   const hostname: string = req.headers.get('host')
+
+  if (req.nextUrl.origin == 'http://localhost:3000') {
+    NextResponse.next()
+    return
+  }
 
   if (hostname.indexOf('app') != -1) {
     NextResponse.next()
